583cf9f999a55182d22a56135a508ae091a86442
onu henry <onuhenry24@gmail.com>
2021-04-26 17:51:54 +0100

    update motivation.rb

--- lib/motivation.rb
+++ lib/motivation.rb
@@ -3,4 +3,22 @@ require 'Json'
 require 'net/http'
 require_relative 'bot'
 
-Class
+class Motivation
+    
+   def initialize
+    @motive = make_motive_request
+   end
+    def random_quote
+    @motive = @motive.sample
+    @motive
+  end
+    
+
+   def make_motive_request
+    urp = URI('https://type.fit/api/quotes')
+    JSON.parse(Net::HTTP.get(urp))
+   end
+
+  
+
+end
