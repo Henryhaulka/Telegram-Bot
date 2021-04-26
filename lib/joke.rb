b3214cdf2211b0bba84043dfee597555a404e52a
onu henry <onuhenry24@gmail.com>
2021-04-26 15:09:59 +0100

    add joke.rb

---
+++ lib/joke.rb
@@ -0,0 +1,17 @@
+require 'telegram/bot'
+require 'net/http'
+require 'json'
+require_relative 'bot.rb'
+
+class JokeLib
+  @item = nil
+
+  def initialize
+    @item = @inquire_a_request
+  end
+
+  def inquire_a_request
+    urp = URI('https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,sexist&type=single')
+    JSON.parse(Net::HTTP.get(urp))
+  end
+end
