939c41db015854bbff9a80e791edffdefb43ceb2
onu henry <onuhenry24@gmail.com>
2021-04-26 15:40:43 +0100

    update bot.rb

--- lib/bot.rb
+++ lib/bot.rb
@@ -6,9 +6,9 @@ class Bot
         token = '1746243344:AAHfoHmw3_i-H5S3aKzLHEcVB02-ILlSieY'
         Telegram::Bot::Client.run(token) do |bot|
             bot.listen do |message|
-                case message.text
+                case message.text.downcase
                    when '/start'
-                    bot.api.send_message(chat_id: message.chat.id, text: "Hello #{message.from.first_name}, Welcome to Motivational quotes and Funny jokes created my ONU HENRY.The chat bot will keep you motivated and fun_filled. Use  /start to start the bot,  /stop to end the bot, /motivate to get a different motivational quotes  or /joke to get a joke everytime you request for any of the options" )
+                    bot.api.send_message(chat_id: message.chat.id, text: "Hello #{message.from.first_name}, Welcome to Motivational quotes and Funny jokes created my ONU HENRY.The chat bot will keep you motivated and fun_filled. Use  /start to start the bot,  /stop to end the bot, /motivate to get a different motivational quotes  or /joke to get a joke everytime you request for any of the options..N.B. inputs are not case sensitive" )
                     when '/stop'
                     bot.api.send_message(chat_id: message.chat.id, text: "Bye #{message.from.first_name},""Hope you had fun and was motivated,do visit another time.", date: message.date)
                     when '/joke'
