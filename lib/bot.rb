bb134975a85bb2b086a916c75fccee936fee5b89
onu henry <onuhenry24@gmail.com>
2021-04-26 15:09:35 +0100

    add bot.rb

---
+++ lib/bot.rb
@@ -0,0 +1,23 @@
+require 'telegram/bot'
+require_relative 'joke.rb'
+
+class Bot
+    def initialize
+        token = '1746243344:AAHfoHmw3_i-H5S3aKzLHEcVB02-ILlSieY'
+        Telegram::Bot::Client.run(token) do |bot|
+            bot.listen do |message|
+                case message.text
+                   when '/start'
+                    bot.api.send_message(chat_id: message.chat.id, text: "Hello #{message.from.first_name}, Welcome to Motivational quotes and Funny jokes created my ONU HENRY.The chat bot will keep you motivated and fun_filled. Use  /start to start the bot,  /stop to end the bot, /motivate to get a different motivational quotes  or /joke to get a joke everytime you request for any of the options" )
+                    when '/stop'
+                    bot.api.send_message(chat_id: message.chat.id, text: "Bye #{message.from.first_name},""Hope you had fun and was motivated,do visit another time.", date: message.date)
+                    when '/joke'
+                      item = JokeLib.new
+                        value = item.inquire_a_request
+                        bot.api.send_message(chat_id: message.chat.id, text: "#{value['joke']}", date: message.date)
+
+                end
+            end
+        end
+    end
+end
