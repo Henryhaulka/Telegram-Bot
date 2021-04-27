2b8b435748caf94238fe49b51f73bd6c494f579e
onu henry <onuhenry24@gmail.com>
2021-04-27 10:26:44 +0100

    add testing for motivation

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -1,3 +1,15 @@
 require_relative '../lib/motivation'
 require_relative '../lib/joke'
 
+describe Motivation do
+    describe '#make_motive_request' do
+    let(:make_request) {Motivation.new}
+    let(:ask_json) {make_request.make_motive_request}
+    it 'Returns a JSON object' do
+        expect(ask_json.class).to eql(Array)
+    end
+
+    it 'Returns a non JSON object' do
+        expect(ask_json.length).to_not eql(0)
+    end
+end
