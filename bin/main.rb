b44747f37656e271be00f308fb1abcdd7c58cfaf
onu henry <onuhenry24@gmail.com>
2021-04-27 11:49:29 +0100

    fix rubocop error

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,5 @@
 require_relative '../lib/bot'
-puts 'Hello! , Welcome to Motivational/Funny telegram bot'
-puts '-----------------------------------------'
-puts 'This  bot will keep you motivated  by encouraging you with motivational quotes'
-puts 'The bot also keeps elated giving jokes'
-puts 'This bot is here to insipre you when your down and unmotivated'
-sleep(0.8)
-puts '------------------------------------------'
+
 puts '...loading the bot is now online... use ctrl-c to stop the bot'
 puts '...give it a few seconds'
 Bot.new
