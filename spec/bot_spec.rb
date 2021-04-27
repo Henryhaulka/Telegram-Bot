f6552012aef56db767ed607026a05bbb38cfdc4b
onu henry <onuhenry24@gmail.com>
2021-04-27 10:34:30 +0100

    update bot_spec.rb

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -3,23 +3,24 @@ require_relative '../lib/joke'
 
 describe Motivation do
     describe '#make_motive_request' do
-    let(:make_request) {Motivation.new}
-    let(:ask_json) {make_request.make_motive_request}
-    it 'Returns a JSON object' do
-        expect(ask_json.class).to eql(Array)
-    end
-
-    it 'Returns a non JSON object' do
-        expect(ask_json.length).to_not eql(0)
-    end
-    describe '#random_quote' do
-        let(:make_quote) {Motivation.new}
-        let(:ask_json) {make_request.random_quote}
-        it 'Returns an Hash' do
-            expect(ask_json.class).to eql(Hash)
+        let(:make_request) {Motivation.new}
+        let(:ask_json) {make_request.make_motive_request}
+        it 'Returns a JSON object' do
+            expect(ask_json.class).to eql(Array)
         end
-        it 'Returns a key and value pair' do
-             expect(ask_json.length).to eql(2)
+
+        it 'Returns a non JSON object' do
+            expect(ask_json.length).to_not eql(0)
+        end
+        describe '#random_quote' do
+            let(:make_quote) {Motivation.new}
+            let(:ask_json) {make_request.random_quote}
+            it 'Returns an Hash' do
+                expect(ask_json.class).to eql(Hash)
+            end
+            it 'Returns a key and value pair' do
+                expect(ask_json.length).to eql(2)
+            end
         end
     end
 end
@@ -34,10 +35,7 @@ describe JokeLib do
         it 'Returns a non Json object' do
             expect(ask_json.length).to_not eql(0)
          end
-
-    end
-
-         
+    end     
     
 end
-end
+
