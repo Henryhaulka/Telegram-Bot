effe1d5b82fa3de5011a9abb331a33e30f3e94a4
onu henry <onuhenry24@gmail.com>
2021-04-27 11:51:03 +0100

    update bot_spec.rb

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -2,40 +2,38 @@ require_relative '../lib/motivation'
 require_relative '../lib/joke'
 
 describe Motivation do
-    describe '#make_motive_request' do
-        let(:make_request) {Motivation.new}
-        let(:ask_json) {make_request.make_motive_request}
-        it 'Returns a JSON object' do
-            expect(ask_json.class).to eql(Array)
-        end
-
-        it 'Returns a non JSON object' do
-            expect(ask_json.length).to_not eql(0)
-        end
-        describe '#random_quote' do
-            let(:make_quote) {Motivation.new}
-            let(:ask_json) {make_request.random_quote}
-            it 'Returns an Hash' do
-                expect(ask_json.class).to eql(Hash)
-            end
-            it 'Returns a key and value pair' do
-                expect(ask_json.length).to eql(2)
-            end
-        end
+  describe '#make_motive_request' do
+    let(:make_request) { Motivation.new }
+    let(:ask_json) { make_request.make_motive_request }
+    it 'Returns a JSON object' do
+      expect(ask_json.class).to eql(Array)
     end
+
+    it 'Returns a non JSON object' do
+      expect(ask_json.length).to_not eql(0)
+    end
+    describe '#random_quote' do
+      let(:make_quote) { Motivation.new }
+      let(:ask_json) { make_request.random_quote }
+      it 'Returns an Hash' do
+        expect(ask_json.class).to eql(Hash)
+      end
+      it 'Returns a key and value pair' do
+        expect(ask_json.length).to eql(2)
+      end
+    end
+  end
 end
 
 describe JokeLib do
-    describe '#inquire_a_request' do
-        let(:request) {JokeLib.new}
-        let(:ask_json) {request.inquire_a_request}
-        it 'Returns a Hash' do
-            expect(ask_json.class).to eql(Hash)
-        end
-        it 'Returns a non Json object' do
-            expect(ask_json.length).to_not eql(0)
-         end
-    end     
-    
+  describe '#inquire_a_request' do
+    let(:request) { JokeLib.new }
+    let(:ask_json) { request.inquire_a_request }
+    it 'Returns a Hash' do
+      expect(ask_json.class).to eql(Hash)
+    end
+    it 'Returns a non Json object' do
+      expect(ask_json.length).to_not eql(0)
+    end
+  end
 end
-
